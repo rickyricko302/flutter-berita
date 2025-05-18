baf6b578614b7e3b37c243d676a0a2a2b10f4557
Ricky Verdiyanto <rickyricko302@gmail.com>
2025-05-18 10:59:39 +0700

    core: remove all for init new project

--- android/app/src/main/kotlin/com/example/udb_news/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.udb_news
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/utils/constants.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color primaryColor({required BuildContext context}) {
-  return Theme.of(context).primaryColor;
-}
-
-Color lightPrimaryColor({required BuildContext context}) {
-  return Theme.of(context).primaryColor.withValues(alpha: 0.1);
-}
